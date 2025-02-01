1cef14555b5a8b1d2fb3d4ad5bdb98799f8b83d6
Hugo <hugopalominogalindo974@gmail.com>
2025-02-01 17:01:15 -0500

    delete method

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ContactService } from '../../service/contact.service';
 import { Contact } from '../../model/contact.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-contact-form',
@@ -23,6 +24,7 @@ export default class ContactFormComponent implements OnInit{
 
   form? : FormGroup;
   contact?: Contact;
+  errors: string[] = [];
 
 
   ngOnInit(): void {
@@ -33,34 +35,41 @@ export default class ContactFormComponent implements OnInit{
           this.contact = contact;
           this.form = this.fb.group({
           name: [contact.name, [Validators.required]],
-          email: [contact.email, [Validators.required]]
+          email: [contact.email, [Validators.required, Validators.email ]]
           });
         }
       )
     } else {
       this.form = this.fb.group({
         name: ['', [Validators.required]],
-        email: ['', [Validators.required]]
-      })
+        email: ['', [Validators.required, Validators.email ]]
+      });
     }
   }
 
   save() {
-    const contactForm =  this.form!.value
-    if (this.contact) {
-      this.contactService.update(this.contact.id, contactForm).subscribe(
-        () => {
-          this.router.navigate(['/'])
-        }
-      );
-    } else {
-      this.contactService.create(contactForm).subscribe(
-        () => {
-          this.router.navigate(['/'])
-        }
-      );
+    if(this.form?.invalid) {
+      this.form?.markAllAsTouched();
+      return;
     }
 
-  }
+    const contactForm =  this.form!.value
+    let request: Observable<Contact>;
 
+    if (this.contact) {
+      request = this.contactService.update(this.contact.id, contactForm);
+    } else {
+      request =  this.contactService.create(contactForm)
+    }
+
+    request.subscribe({
+      next: () => {
+        this.errors = [];
+        this.router.navigate(['/'])
+      },
+      error: (response) => {
+        this.errors = response.error.errors
+      }
+    });
+  }
 }
